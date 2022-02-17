84de0810f70645f9a3375281963e02451fb79c72
Bichi <gnzlz.bianca@gmail.com>
2022-02-16 19:38:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+//alert('Hola desde un archivo externo');
+
+//este es un mensaje de la consola
+console.log('Hola Consola');
+
+var x;
+
+x = "3.1416";
+
+var publicdata = "Hola";
+
+var nombre = "Sergio";
